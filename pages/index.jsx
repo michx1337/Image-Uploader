a150ee7ac717621702c96b4951d03ea4b835af78
Igor Kowalczyk <majonezexe@protonmail.com>
2022-07-16 23:10:31 +0200

    Fix classes

--- pages/index.jsx
+++ pages/index.jsx
@@ -26,20 +26,20 @@ export default function Main(props) {
    <Container>
     <div className="flex h-screen flex-col items-center justify-center">
      <div className="gap-6 rounded-md border border-slate-500/20 bg-slate-900/70 shadow-md backdrop-blur">
-      <div class="flex-none border-b border-slate-500/30">
-       <div class="flex h-8 items-center space-x-1.5 px-3">
-        <div class="h-2.5 w-2.5 rounded-full bg-slate-600"></div>
-        <div class="h-2.5 w-2.5 rounded-full bg-slate-600"></div>
-        <div class="h-2.5 w-2.5 rounded-full bg-slate-600"></div>
+      <div className="flex-none border-b border-slate-500/30">
+       <div className="flex h-8 items-center space-x-1.5 px-3">
+        <div className="h-2.5 w-2.5 rounded-full bg-slate-600"></div>
+        <div className="h-2.5 w-2.5 rounded-full bg-slate-600"></div>
+        <div className="h-2.5 w-2.5 rounded-full bg-slate-600"></div>
        </div>
       </div>
       <div className="p-8">
        <h1 className="pb-5 font-poppins text-5xl">{!createObjectURL ? "Select your image" : "Now upload it! 🎉"}</h1>
        <div>
         <span className="text-sm text-slate-400">PNG, JPG and GIF (Max 100mb)</span>
-        <div class="mt-2 flex items-center space-x-6 rounded-md bg-slate-700/25 p-3 shadow-md">
-         <label class="block">
-          <input type="file" class="block w-full cursor-copy text-sm text-slate-400 file:mr-4 file:cursor-pointer file:rounded-md file:border-0 file:bg-[#2869FF] file:py-2 file:px-4 file:text-sm file:font-semibold file:text-white file:duration-200 hover:file:bg-[#2355c8]" onChange={uploadToClient} />
+        <div className="mt-2 flex items-center space-x-6 rounded-md bg-slate-700/25 p-3 shadow-md">
+         <label className="block">
+          <input type="file" className="block w-full cursor-copy text-sm text-slate-400 file:mr-4 file:cursor-pointer file:rounded-md file:border-0 file:bg-[#2869FF] file:py-2 file:px-4 file:text-sm file:font-semibold file:text-white file:duration-200 hover:file:bg-[#2355c8]" onChange={uploadToClient} />
          </label>
         </div>
         <div className="flex items-center">
